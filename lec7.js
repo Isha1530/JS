7bcf62948b38eb8a23ffc6693923bc59cc3a9dbe
Isha1530 <147532644+Isha1530@users.noreply.github.com>
2025-05-19 11:41:22 +0530

    Add files via upload

---
+++ lec7.js
@@ -0,0 +1,49 @@
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log("id: ", id);
+
+// div.setAttribute("id", "new-id");
+
+// let newId = div.getAttribute("id");
+// console.log("new id: ", newId);
+
+// let div = document.querySelector("div");
+// div.style.backgroundColor = "yellow";
+// div.style.color = "red";
+// div.style.fontSize = "24px";
+// div.innerText = "Hello World!";
+
+// insert a new element
+
+// let newbtn = document.createElement("button");
+// newbtn.textContent = "Click me";
+
+// let parentDiv = document.querySelector("p");
+// parentDiv.appendChild(newbtn);
+// parentDiv.prepend(newbtn);
+// parentDiv.before(newbtn);
+// parentDiv.after(newbtn);
+
+// let heading = document.createElement("h1");
+// heading.textContent = "Hello World!";
+
+// let body = document.querySelector("body");
+// body.prepend(heading);
+
+// // remove element
+
+// newbtn.remove();
+// heading.remove();
+
+// practice set
+
+// let newbtn = document.createElement("button");
+// newbtn.innerText = "Click me";
+// newbtn.style.backgroundColor = "red";
+// newbtn.style.color = "white";
+// document.querySelector("body").prepend(newbtn);
+
+let para = document.querySelector("p");
+para.classList.add("new-class"); 
